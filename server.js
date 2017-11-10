ab244db9b7a4fc26753583c9ea44bf4d78ad5d26
Cable <D01587937@dvuadmin.net>
2017-11-10 16:01:28 -0400

    remove env

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ Variables
 var username = "admin";
 var password = "THEpassw0rd";
 var server_port = process.env.OPENSHIFT_NODEJS_PORT; // || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP; // || "127.0.0.1"
+var server_ip_address = OPENSHIFT_NODEJS_IP; // || "127.0.0.1"
 
 function get_AuthRoot(username, password) {
     //var root_url =  "http://"+username+":"+password+"@localhost:8080/cyclos/pelican/web-rpc/";
