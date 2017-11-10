a87abea6c21c1974cd3ba59a900d13c0558d4c28
Kendall Cable <mrkencable@gmail.com>
2017-11-10 00:35:39 -0400

    updated for async function

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ Variables
 
 var username = "admin";
 var password = "THEpassw0rd";
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8181;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"
 
 
