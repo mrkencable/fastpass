0efe14f8b411a8a932eb469e5c254e451fbf25c7
Cable <D01587937@dvuadmin.net>
2017-11-10 15:45:18 -0400

    use openstack env variables for route

--- server.js
+++ server.js
@@ -106,8 +106,8 @@ Variables
 
 var username = "admin";
 var password = "THEpassw0rd";
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"
+var server_port = process.env.OPENSHIFT_NODEJS_PORT; // || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP; // || "127.0.0.1"
 
 
 
